923f1067d98dedd3a14bce4aaaf7d0778ad3daa3
alichan <alice.hitugi@gmail.com>
2021-09-05 20:10:24 +0900

    vuetify削除

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-// import vuetify from './plugins/vuetify'
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.config.productionTip = false
@@ -11,7 +10,6 @@ const vuetify = new Vuetify({});
 
 new Vue({
     router,
-    // vuetify,
     vuetify,
     render: h => h(App),
 }).$mount('#app')
