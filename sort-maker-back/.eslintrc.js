e929b55b0e87c66694d1f61aaf3a726354153afc
alichan <alice.hitugi@gmail.com>
2021-09-20 04:49:37 +0900

    firebaseにユーザー登録してみた

--- sort-maker-back/.eslintrc.js
+++ sort-maker-back/.eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    env: { node: true },
-    extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- sort-maker-back/routes/user-register.js
+++ sort-maker-back/routes/user-register.js
@@ -3,9 +3,26 @@ const firebase = require('firebase')
 const router = express.Router()
 
 router.get('/user-register', function (req, res) {
-    res.json({
-        message: 'hello world',
-    })
+    const config = {
+        apiKey: 'AIzaSyCEAHNzKTE6YnO4UU1-Ik_Vzq20oWJ9Ftg',
+        authDomain: 'sort-maker-f6861.firebaseapp.com',
+        projectId: 'sort-maker-f6861',
+        storageBucket: 'sort-maker-f6861.appspot.com',
+        messagingSenderId: '1076993903791',
+        appId: '1:1076993903791:web:5398d1e3ac45046835185a',
+        measurementId: 'G-ZLX17697ZS',
+    }
+    firebase.initializeApp(config)
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword('sample@sample.com', 'sample')
+        .then((user) => {
+            res.send('Create account: ', user.email)
+        })
+        .catch((error) => {
+            res.send(error.message)
+        })
 })
 
 module.exports = router
