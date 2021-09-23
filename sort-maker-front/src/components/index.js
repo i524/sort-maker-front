b5a57c2a457d15d9bfddf71a774b30fbabd36313
alichan <alice.hitugi@gmail.com>
2021-09-23 19:05:37 +0900

    エラーの修正

--- sort-maker-front/src/components/index.js
+++ sort-maker-front/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as CustomButton } from './CustomButton.vue'
-export { default as CustomForm } from './CustomForm.vue'
 export { default as CustomPagenation } from './CustomPagination.vue'
 export { default as CustomTextField } from './CustomTextField.vue'
 export { default as Footer } from './Footer.vue'
