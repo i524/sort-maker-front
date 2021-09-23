6b467fe7f6236ae33249f2b0e39cff0140e8d147
alichan <alice.hitugi@gmail.com>
2021-09-23 16:49:04 +0900

    UserRegisterのバリデーションを外部関数化

--- sort-maker-front/src/views/RegisterUser.vue
+++ sort-maker-front/src/views/RegisterUser.vue
@@ -41,11 +41,14 @@
 
 <script>
 import { CustomTextField, CustomButton } from '../components'
-
-const regHalf = new RegExp(/^[a-zA-Z0-9!-/:-@¥[-`{-~]+$/)
-const regEmail = new RegExp(
-    /^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/
-)
+import {
+    required,
+    isValidEmailLength,
+    isMatchedRegHalf,
+    isMatchedEmail,
+    isValidPasswordLength,
+    isSamePassword,
+} from '../common_functions/validation'
 
 export default {
     components: {
@@ -57,21 +60,12 @@ export default {
             email: '',
             password: '',
             rePassword: '',
-            required: (value) => !!value || '必ず入力してください',
-            isValidEmailLength: (value) =>
-                (1 <= value.length && value.length <= 255) ||
-                '1文字以上255文字以下で入力してください',
-            isMatchedRegHalf: (value) =>
-                regHalf.test(value) || '半角数字記号で入力してください',
-            isMatchedEmail: (value) =>
-                regEmail.test(value) ||
-                'メールアドレスの形式で入力してください',
-            isValidPasswordLength: (value) =>
-                (6 <= value.length && value.length <= 255) ||
-                '6文字以上255文字以下で入力してください',
-            isSamePassword: (value) =>
-                value === this.password ||
-                '再入力したパスワードがパスワードと一致していません',
+            required: required,
+            isValidEmailLength: isValidEmailLength,
+            isMatchedRegHalf: isMatchedRegHalf,
+            isMatchedEmail: isMatchedEmail,
+            isValidPasswordLength: isValidPasswordLength,
+            isSamePassword: (value) => isSamePassword(value, this.password),
         }
     },
     methods: {
