3a8e8de163e6647453b8cce2aa7d94cffa59e46a
alichan <alice.hitugi@gmail.com>
2021-09-27 02:35:04 +0900

    Loginページ更新

--- sort-maker-back/common_functions/initializeApp.js
+++ sort-maker-back/common_functions/initializeApp.js
@@ -1,12 +1,12 @@
 const initializeApp = function (firebase) {
     const config = {
-        apiKey: 'AIzaSyAVQtyr4Qnc2bdlU2irOWWQT4DX6DnIIDE',
-        authDomain: 'sort-maker-97261.firebaseapp.com',
-        projectId: 'sort-maker-97261',
-        storageBucket: 'sort-maker-97261.appspot.com',
-        messagingSenderId: '17511495381',
-        appId: '1:17511495381:web:8d02a5f106f2e12026145c',
-        measurementId: 'G-FN10FEGE2W',
+        apiKey: 'AIzaSyAzZ5T7Y7n5bAlB9GERaBDpOZW9rUEY_-8',
+        authDomain: 'sort-maker-59e03.firebaseapp.com',
+        projectId: 'sort-maker-59e03',
+        storageBucket: 'sort-maker-59e03.appspot.com',
+        messagingSenderId: '1072382239411',
+        appId: '1:1072382239411:web:ce106331825496f4d686c6',
+        measurementId: 'G-8C3MJ3HPQZ',
     }
     if (firebase.apps.length === 0) {
         firebase.initializeApp(config)
