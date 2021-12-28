12586f78b0f7b9f1a0741640d442d6fb3ded0e9d
alichan <alice.hitugi@gmail.com>
2021-12-29 01:06:54 +0900

    sort-maker-frontディレクトリを一つなくした

--- sort-maker-front/src/plugins/index.js
+++
@@ -1,1 +0,0 @@
-export { default as vuetify } from './vuetify'
--- sort-maker-front/src/store/modules/index.js
+++
@@ -1,3 +0,0 @@
-export { default as auth } from './auth'
-export { default as sort } from './sort'
-export { default as ui } from './ui'
